e80ea1105e7f9c7eda068b810b5bc7b1d17ecb84
sherryrich <ricsherry@gmail.com>
2022-04-28 10:18:37 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+print("""
+  ___   _   _  _ ___  ___ ___  ___ _  _   _ _____ ___ _  _ 
+ | _ ) /_\ | \| |   \| __| _ \/ __| \| | /_\_   _/ __| || |
+ | _ \/ _ \| .` | |) | _||   /\__ \ .` |/ _ \| || (__| __ |
+ |___/_/ \_\_|\_|___/|___|_|_\|___/_|\_/_/ \_\_| \___|_||_|
+                                                           
+""")
+
+print("Welcome to Bandersnatch")
+print("Choose your own adventure")
