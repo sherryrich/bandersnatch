ad59cac835bb8da1ae935d53d38dab00e106b8cb
sherryrich <ricsherry@gmail.com>
2022-04-29 08:48:39 +0000

    updated colors wrong correct and incorrect paths

--- run.py
+++ run.py
@@ -41,21 +41,22 @@ user_name = input("What is your name?\n").capitalize()
 print()
 print("Welcome " + user_name + "!. \nlets start your adventure shall we?\nRemember to choose the right path to make 'Bandersnatch' a reality.")
 print()
+# write line do they accept to play or not?
 choice1 = input("You\'re traveling to the company and are at a crossraods, which way do\nyou want to go? Type 'left' or 'right'.\n").lower()
 print()
 if choice1 == "left":
-    choice2 = input("Good choice. You\'re now at the train station but the train is running late, do you wait for the train or get a taxi to get to Tuckersoft Ltd offices? Type 'wait' to take the train or type 'taxi' to hail a taxi\n").lower()
+    choice2 = input(F"{Fore.GREEN}Good choice. You\'re now at the train station but the train is running late, do you wait for the train or get a taxi to get to Tuckersoft Ltd offices? Type 'wait' to take the train or type 'taxi' to hail a taxi\n").lower()
     if choice2 == "wait":
-        choice3 = input("Good choice. You took the train and made it on time for the meeting. Tuckersoft like the game and there are 3 offers on the table. Which one do you choose? Type '1' to take offer 1, Type '2' to take offer 2, Type '3' to take offer 3\n").lower()
+        choice3 = input(F"{Fore.GREEN}Good choice. You took the train and made it on time for the meeting. Tuckersoft like the game and there are 3 offers on the table. Which one do you choose? Type '1' to take offer 1, Type '2' to take offer 2, Type '3' to take offer 3\n").lower()
         if choice3 == "1":
-            print("Bad choice, you got fired and they stole your idea. Game over")
+            print(F"{Fore.RED}Bad choice, you got fired and they stole your idea. Game over")
         elif choice3 == "2":
-            print("Bad choice, they take your idea and it fails to get any sales due to poor reviews. Game over")
+            print(F"{Fore.RED}Bad choice, they take your idea and it fails to get any sales due to poor reviews. Game over")
         elif choice3 == "3":
             print(F"{Fore.GREEN}Good choice. You politely decline their offer and decide to go alone, you win and the game is a huge worldwide sucess. You Win")
         else:
-            print("You failed to take any offer. Game over")
+            print(F"{Fore.RED}You failed to take any offer. Game over")
     else:
-        print("Wrong choice. You have missed the meeting. Tuckersoft Ltd are no longer interested. Game Over.")
+        print(F"{Fore.RED}Wrong choice. You have missed the meeting. Tuckersoft Ltd are no longer interested. Game Over.")
 else:
-    print("Wrong choice. You just got hit by a bus and died. Game Over.")
+    print(F"{Fore.RED}Wrong choice. You just got hit by a bus and died. Game Over.")
