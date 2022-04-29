ef2db36f3c1ba9448a4930215b21e6d9f28dd6e0
sherryrich <ricsherry@gmail.com>
2022-04-29 15:25:11 +0000

    added leadtime question

--- run.py
+++ run.py
@@ -23,7 +23,7 @@ print(F"""{Fore.RED}
 # credit https://patorjk.com/software/taag/#p=author&f=Small&t=Bandersnatch
 
 
-print(F"{Fore.YELLOW}\n\tWelcome to Bandersnatch \U0001F47E !\n")
+print(F"{Fore.YELLOW}\n\t\tWelcome to Bandersnatch \U0001F47E !\n")
 typewrite("You just developed a new computer game 'Bandersnatch'.")
 typewrite("You must to get to the office of Tuckersoft Ltd to sell the game.")
 print()
@@ -43,9 +43,9 @@ print()
 choice1 = input("You\'re traveling to the company and are at a crossraods, which way do\nyou want to go? Type 'left' or 'right'.\n").lower()
 print()
 if choice1 == "left":
-    choice2 = input(F"{Fore.GREEN}Good choice. You\'re now at the train station but the train is running late,\ndo you wait for the train or get a taxi to get to Tuckersoft offices?\nType 'wait' to take the train or type 'taxi' to hail a taxi\n").lower()
+    choice2 = input(F"{Fore.GREEN}Good choice. You\'re now at the train station but the train is running late.\nDo you wait for the train or get a taxi to get to Tuckersoft offices?\nType 'wait' to take the train or type 'taxi' to hail a taxi\n").lower()
     if choice2 == "wait":
-        choice3 = input(F"{Fore.GREEN}Good choice. You took the train and made it on time for the meeting. Tuckersoft like the game and there are 3 offers on the table. Which one do you choose? Type '1' to take offer 1, Type '2' to take offer 2, Type '3' to take offer 3\n").lower()
+        choice3 = input(F"{Fore.GREEN}Good choice. You took the train and made it on time for the meeting. Tuckersoft like the game.\nThere are 3 offers on the table. Which one do you choose?\nType '1' to take offer 1\nType '2' to take offer 2\nType '3' to take offer 3\n").lower()
         if choice3 == "1":
             print(F"{Fore.RED}Bad choice, you got fired and they stole your idea. Game over")
         elif choice3 == "2":
@@ -58,3 +58,13 @@ if choice1 == "left":
         print(F"{Fore.RED}Wrong choice. You have missed the meeting.\nTuckersoft Ltd are no longer interested.\nGame Over.")
 else:
     print(F"{Fore.RED}Wrong choice. You just got hit by a bus and died. Game Over.")
+
+lead_time = input("The company ask how many weeks will it take you to finish the completed version?")
+if lead_time:
+	lead_time = int(lead_time)
+	if lead_time >= 21:
+	    print(F"{Fore.RED}Thats too long, we cant do a deal. Game Over.")
+	elif lead_time <= 20:
+	    print(F"{Fore.GREEN}Ok you got yourself a deal.")
+	else: 
+	    print("Please enter number of weeks")
