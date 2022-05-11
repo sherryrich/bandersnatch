0920bdb9b09fa7a351d06ab14888c1a8efbeeb18
sherryrich <ricsherry@gmail.com>
2022-05-11 17:36:46 +0000

    updated color and flowchart responses and changed wording

--- run.py
+++ run.py
@@ -14,9 +14,9 @@ def typewrite(string):
         sys.stdout.flush()
         time.sleep(0.1)
 
-print(f"{Fore.RED}{logo_art}")
+print(f"{Fore.YELLOW}{logo_art}")
 
-print(F"{Fore.YELLOW}\n\t\tWelcome to Bandersnatch \U0001F47E !\n")
+print(F"{Fore.RED}\n\t\tWelcome to Bandersnatch \U0001F47E !\n")
 typewrite("You just developed a new computer game 'Bandersnatch'.\n")
 typewrite("You must to get to the office of Tuckersoft Ltd to sell the game.")
 print()
@@ -26,8 +26,10 @@ play_game = input("Do you wish to play? Y/N\n").capitalize()
 if play_game == "Y":
     typewrite("Ok lets start your adventure")
 else:
-    print("Goodbye")
+    print(F"{Fore.RED}Thank you for playing. Goodbye")
     print(f"{Fore.RED}{game_over_art}")
+    quit()
+
 print()
 user_name = input("What is your name?\n").capitalize()
 print()
@@ -35,7 +37,7 @@ typewrite("Welcome " + user_name + "!\n")
 typewrite("Remember, choose the right path to make 'Bandersnatch' a reality.")
 print()
 
-typewrite("You\'re traveling to the company offices and are at a crossraods\n")
+typewrite("You\'re traveling to Tuckersoft offices and are at a crossroads.\n")
 typewrite("Which way do you want to go?\n")
 choice1 = input("Type 'left' or 'right'\n").lower()
 print()
@@ -43,8 +45,8 @@ if choice1 == "left":
     print(F"{Fore.GREEN}Good choice " + user_name + "")
     typewrite("You\'re at the train station but the train is running late.\n")
     typewrite("Do you wait for a train or get a taxi to Tuckersoft offices?\n")
-    choice2 = input("Type 'wait' for train or 'taxi' take a taxi\n").lower()
-    if choice2 == "wait":
+    choice2 = input("Type 'train' for a train or 'taxi' for a taxi\n").lower()
+    if choice2 == "train":
         print(F"{Fore.GREEN}Good choice " + user_name + "")
         typewrite("You took the train and made it on time for the meeting.\n")
         typewrite("Tuckersoft like the game. 3 offers are now on the table.\n")
@@ -53,28 +55,34 @@ if choice1 == "left":
         typewrite("Type '3' to take offer 3\n")
         choice3 = input("Which one do you choose?\n").lower()
         if choice3 == "1":
-            print(F"{Fore.RED}Bad choice\n")
-            typewrite("Tuckersoft steal your idea.\n")
-            print("Game over")
+            print(F"{Fore.RED}Bad choice")
+            print(F"{Fore.RED}Tuckersoft steal your idea.")
+            print(F"{Fore.RED}Sell the game directly themselves.\n")
+            print(f"{Fore.RED}{game_over_art}")
+            quit()
+
         elif choice3 == "2":
-            print(F"{Fore.YELLOW}OK, Tuckersoft are interested\n")
+            print(F"{Fore.YELLOW}OK, Tuckersoft are still interested\n")
             typewrite("Tuckersoft ask how many weeks will it take you\n")
-            typewrite("Less than 20 weeks or longer?\n")
-            lead_time = input("To finish the completed version?\n")
+            typewrite("to finish the completed version?\n")
+            lead_time = input("Type a number between 1-40\n")
             if lead_time:
                 lead_time = int(lead_time)
                 if lead_time >= 21:
-                    typewrite("Thats too long, Tuckersoft refuse. Game Over.")
+                    print(F"{Fore.RED}Bad choice")
+                    print(f"{Fore.RED}Thats too long, Tuckersoft refuse.")
+                    print(f"{Fore.RED}{game_over_art}")
+                    quit()
                 if lead_time <= 20:
-                    typewrite("Congratulations, you got yourself a deal.")
+                    print(f"{Fore.YELLOW}Congratulations, you got a deal.")
+                    print(f"{Fore.YELLOW}next time try another offer?")
                     print(f"{Fore.YELLOW}{deal_art}")
                 else:
                     typewrite("Please enter number of weeks")
         elif choice3 == "3":
             print(F"{Fore.GREEN}Good choice " + user_name + "")
-            typewrite("You decline this offer and decide to go alone\n")
-            typewrite("Congratulations the game is a huge worldwide sucess.\n")
-            print(F"{Fore.GREEN}You Win " + user_name + "")
+            print(F"{Fore.GREEN}You decline offer 3 and decide to go alone")
+            print(F"{Fore.GREEN}Bandersnatch is a huge worldwide sucess.\n")
             print(f"{Fore.GREEN}{win_art}")
             game_is_finished = False
             lives = len(stages) - 1
@@ -111,26 +119,18 @@ if choice1 == "left":
                         typewrite("Thank you for playing Bandersnatch.")
 
                     print(stages[lives])
+                    quit()
 
         else:
             print(F"{Fore.RED}You failed to take any offer. Game over")
     else:
         print(F"{Fore.RED}Wrong choice.")
-        print("You have missed the meeting.\n")
-        print("Tuckersoft Ltd are no longer interested.\n")
-        print("Game Over.")
+        print(F"{Fore.RED}You have missed the meeting.\n")
+        print(F"{Fore.RED}Tuckersoft Ltd are no longer interested.\n")
+        print(f"{Fore.RED}{game_over_art}")
+        quit()
 else:
     print(F"{Fore.RED}Wrong choice.")
-    print("You just got hit by a bus and died. Game Over.")
-
-# print("Tuckersoft ask how many weeks will it take you\n")
-# lead_time = input("To finish the completed version?\n")
-# if lead_time:
-# 	lead_time = int(lead_time)
-# 	if lead_time >= 21:
-# 	    print(F"{Fore.RED}Thats too long, we cant do a deal. Game Over.")
-# 	elif lead_time <= 20:
-# 	    print(F"{Fore.GREEN}Congratulations, you got yourself a deal.")
-# 	    print(f"{Fore.GREEN}{deal_art}")
-# 	else:
-# 	    print("Please enter number of weeks")
+    print(F"{Fore.RED}You just got hit by a bus and died.")
+    print(f"{Fore.RED}{game_over_art}")
+    quit()
